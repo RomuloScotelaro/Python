78fb925148beb037decd7f331b464333996e3727
Romulo Scotelaro <romulo.bjj3@gmail.com>
2022-11-25 13:59:24 -0300

    Desafio 6

---
+++ Desafios/ds05/solução.py
@@ -0,0 +1,2 @@
+n=int(input('Digite um número:'))
+print('Analisando o valor {}, seu antencessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
---
+++ Desafios/ds06/ds006.py
@@ -0,0 +1,5 @@
+n=int(input('Informe um numero:'))
+d=n*2
+t=n*3
+q=n**(1/2)
+print ('o numero é {} e seu dobro é {} seu triplo é {} e a sua raiz quadrada é {:.2f}'.format(n,d,t,q))
